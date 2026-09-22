0bc139bee659565982166dda372c6b5ff3d5a29d
JBuilds <goldmanyanky72@gmail.com>
2026-09-22 01:54:13 -0400

    chore: bump version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
     applicationId = "com.jbuilds.battfo"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
-    versionName = "1.0.1"
+    versionCode = 3
+    versionName = "1.1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
